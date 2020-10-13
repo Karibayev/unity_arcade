2e74d1fb2caa81de2ef7b2935d90352fc96e254a
Karibayev <kazakh.blogs@gmail.com>
2020-10-14 01:45:50 +0600

    Initial commit

---
+++ Assets/Scripts/ExplodeCubes.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ExplodeCubes : MonoBehaviour {
+	private void OnCollisionEnter(Collision collision) {
+		if(collision.gameObject.tag == "Cube"){
+			for(int i == collision.transform.childCount - 1; i >= 0; i--){
+				Transform child = collision.transform.GetChild(i);
+				child.gameObject.AddComponent<Rigidbody>();
+				child.gameObject.GetComponent<Rigidbody>().AddExplosionForce(70f, Vector3.up, 5f);
+				childSetParent(null);
+
+			}
+			Destroy(collision.gameObject);
+			_collisionSet = true;
+
+		}
+	}
+    
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+public class GameController : MonoBehaviour {
+ 	private CubePos nowCube = new CubePos( 0, 1, 0);
+	public float cubeChallengePlaceSpeed = 0.5f;
+	public Transform cubeToPlace;
+	private List<Vector3> allCubePositions = new List<Vector3> {
+		new Vector3(0, 0, 0),
+		new Vector3(1, 0, 0),
+		new Vector3(-1, 0, 0),
+		new Vector3(0, 1, 0),
+		new Vector3(0, 0, 1),
+		new Vector3(0, 0, -1),
+		new Vector3(1, 0, 1),
+		new Vector3(-1, 0, -1),
+		new Vector3(-1, 0, 1),
+		new Vector3(1, 0, -1)
+
+	};
+		private void Start() {
+		StartCoroutine(ShowCubePlace());
+
+	}
+
+	IEnumerator ShowCubePlace() {
+		while(true) {
+			SpawnPositions();
+			yield return new WaitForSeconds(cubeChallengePlaceSpeed);
+
+
+		}
+	}
+	private void SpawnPositions(){
+		List<Vector3> positions = new List<Vector3>();
+		if (IsPositionEmpty(new Vector3(nowCube.x + 1, nowCube.y, nowCube.z))) 
+			positions.Add(new Vector3(nowCube.x + 1, nowCube.y, nowCube.z)); 
+		else if (IsPositionEmpty(new Vector3(nowCube.x - 1, nowCube.y, nowCube.z))) 
+			positions.Add(new Vector3(nowCube.x - 1, nowCube.y, nowCube.z)); 
+		else if (IsPositionEmpty(new Vector3(nowCube.x, nowCube.y + 1, nowCube.z))) 
+			positions.Add(new Vector3(nowCube.x, nowCube.y + 1, nowCube.z));
+		else if (IsPositionEmpty(new Vector3(nowCube.x, nowCube.y - 1, nowCube.z))) 
+			positions.Add(new Vector3(nowCube.x, nowCube.y - 1, nowCube.z)); 
+		else if (IsPositionEmpty(new Vector3(nowCube.x, nowCube.y, nowCube.z + 1 ))) 
+			positions.Add(new Vector3(nowCube.x, nowCube.y, nowCube.z + 1)); 
+		else if (IsPositionEmpty(new Vector3(nowCube.x, nowCube.y, nowCube.z - 1 ))) 
+			positions.Add(new Vector3(nowCube.x, nowCube.y, nowCube.z - 1)); 
+		 
+
+		cubeToPlace.positions = positions[UnityEngine.Random.Range(0, pos.Count)];
+		
+
+
+	}
+	private bool IsPositionEmpty(Vector3 targetPos){
+		if(targetPos.y == 0)
+			return false;
+		foreach (Vector3 pos in allCubePositions){
+			if(pos.x == targetPos.x && pos.y == targetPos.y && pos.z == targetPos.z)
+			return false;
+		}
+
+	}
+
+
+
+struct CubePos {
+	public int x, y, z;
+
+	public CubePos(int x, int y, int z){
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	public Vector3 getVector() {
+		return new Vector3(x, y, z);
+	}
+	public void setVector(Vector3 targetPos) {
+		x = Convert.ToInt32(pos.x);
+		y = Convert.ToInt32(pos.y);
+		z = Convert.ToInt32(pos.z);
+	}
+}
